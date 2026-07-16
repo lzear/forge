5e69f1d93926543e0b15cef16cb682a5128f34e6
lzear <antoine.clausse@gmail.com>
2026-07-16 22:53:09 +0200

    chore: ncu

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,14 @@ import lzearConfig from '@lzear/forge/eslint'
 
 const base = await lzearConfig({ local: 'lzear' })
 
-const config: Linter.Config[] = [...base]
+const config: Linter.Config[] = [
+  ...base,
+  {
+    rules: {
+      'package-json/no-nested-exports': 0,
+      'package-json/no-workspace-protocol-in-published-package': 0,
+    },
+  },
+]
 
 export default config
